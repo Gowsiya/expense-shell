f89adae5efec53bdb330449cc97496f6272e1253
Gowsiya <50201437+Gowsiya@users.noreply.github.com>
2025-02-17 10:19:24 +0530

    EXPENSE SHELL

--- mysql.sh
+++ mysql.sh
@@ -39,7 +39,7 @@ VALIDATE $? "Enabling MySQL Server"
 systemctl start mysqld &>>$LOG_FILE_NAME
 VALIDATE $? "Starting MySQL Server"
 
-mysql -h mysql.gsdevops.online -u root -pExpenseApp@1 -e 'show databases' &>>LOG_FILE_NAME
+mysql -h mysql.gsdevops.online -u root -pExpenseApp@1 -e 'show databases;' &>>LOG_FILE_NAME
 if [ $? -ne 0 ]
 then
     echo "Mysql root password not setup" &>>LOG_FILE_NAME
